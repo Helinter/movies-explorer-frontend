061601068879117162f7560de615c6269405de49
Helinter <helarandaragan999@gmail.com>
2023-11-24 19:45:09 +0400

    файловая структура фронтенда

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main__header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default Main;
