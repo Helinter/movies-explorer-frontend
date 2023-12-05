efe521a4466f56d8d2ca8a0cc59ee83966c3db8c
Helinter <helarandaragan999@gmail.com>
2023-12-05 22:32:11 +0400

    пофикшен перенос строки в promo__container__title

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,8 +3,8 @@ function Promo() {
   return (
     <div className="promo">
       <div className="promo__container">
-<h1 className="promo__container__title">Учебный проект студента факультета<br></br>Веб-разработки.</h1>
-<p className="promo__container__subtitle">Листайте ниже, чтобы узнать больше про этот<br></br>проект и его создателя.</p>
+<h1 className="promo__container__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+<p className="promo__container__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
 
             <button className="promo__container__button">Узнать больше</button>
 
